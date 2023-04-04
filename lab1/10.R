000eb0a562488190030af8dc740687f678124999
adiaioana <79255021+adiaioana@users.noreply.github.com>
2023-04-04 11:30:47 +0300

    functie 10.R

--- lab1/10.R
+++ lab1/10.R
@@ -1,8 +1,11 @@
-n=5
+zece=function(n,k){
 set.seed(0) #make this example reproducible
 vec=vector(length=n)
 for(i in 1:n) {
-  vec[i]=dpois(x=i,lambda=2)
+  vec[i]=dpois(x=i,lambda=k)
 }
 #plot (k, vec, type = "l", ylab = "log density")
 barplot(vec)
+}
+
+zece(2,2)
